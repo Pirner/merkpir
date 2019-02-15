a0c3a6e419066f3a45eed3f954e6679fe44a73f6
Stephan <ga58max@mytum.de>
2019-02-15 01:08:59 +0100

    add scale vector function

--- neural_network/src/arithmetic/vectors.rs
+++ neural_network/src/arithmetic/vectors.rs
@@ -5,11 +5,22 @@ pub fn add_vectors(x: &[f64], y: &[f64]) -> Vec<f64> {
     if x.len() == y.len() {
 
         for i in 0..x.len() {
-            v.push(x[i] + y[i])
+            v.push(x[i] + y[i]);
         }
 
     } else {
         panic!("Error in add_vectors, vectors must be same size but x.len is {} and y.len is {}", x.len(), y.len());
     }
+    v
+}
+
+pub fn scale_vector(x: &[f64], s: f64) -> Vec<f64> {
+
+    let mut v: Vec<f64> = Vec::new();
+
+    for i in 0..x.len() {
+        v.push(x[i] * s);
+    }
+    /*return scaled vector*/
     v
 }
--- neural_network/src/main.rs
+++ neural_network/src/main.rs
@@ -3,11 +3,12 @@ mod arithmetic;
 
 
 fn main() {
-    println!("Hello, world!");
+    println!("Test functions for neural network WIP!");
 
     let xs: [f64; 6] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
     let ys: [f64; 6] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
 
     println!("{:?}", arithmetic::vectors::add_vectors(&xs, &ys));
 
+    println!("{:?}", arithmetic::vectors::scale_vector(&xs, -3.0));
 }
