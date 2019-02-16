96f2f60f709067e2f63185388781adcd2f8fd561
Stephan <ga58max@mytum.de>
2019-02-17 00:42:48 +0100

    refactor add vector function to use iterators

--- neural_network/src/arithmetic/vectors.rs
+++ neural_network/src/arithmetic/vectors.rs
@@ -1,18 +1,23 @@
 /* vector functions */
 pub fn add_vectors(x: &[f64], y: &[f64]) -> Vec<f64> {
 
+    /* return vector*/
     let mut v: Vec<f64> = Vec::new();
-    if x.len() == y.len() {
 
-        for i in 0..x.len() {
-            v.push(x[i] + y[i]);
+    if x.len() == y.len() {
+        for it in x.iter().zip(y.iter()) {
+            let (xi, yi) = it;
+            v.push(xi + yi);
         }
 
+    /* only vectors of the same length can be added */
     } else {
         panic!("Error in add_vectors, vectors must be same size but x.len is {} and y.len is {}", x.len(), y.len());
     }
+    /* return the added vector */
     v
 }
+
 
 pub fn scale_vector(x: &[f64], s: f64) -> Vec<f64> {
 
